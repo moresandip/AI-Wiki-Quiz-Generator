bb0af89f75664cb25afad0e26ae34e087f2acad0
Sandip More <moresandip@gmail.com>
2025-11-08 13:58:39 +0530

    Add frontend code files and update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -4,3 +4,5 @@ start "Backend" cmd /k "cd backend && python -m uvicorn main:app --reload"
 
 echo Starting frontend server...
 start "Frontend" cmd /k "cd frontend && npm start"
+
+@REM .\run_app.bat
